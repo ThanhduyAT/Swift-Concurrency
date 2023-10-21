fe97d2cfa8b8e010d561973273dfedaf21f20646
ThanhDuyAT <thanhduy2198@gmail.com>
2023-10-21 21:53:18 +0700

    Initial Source

---
+++ SwiftConcurrency/SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Thanh Duy on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
